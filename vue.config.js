bfad4ec5fc65f5a38eb4b39e142b4ca387e6b6a8
Your Name <you@example.com>
2022-01-11 00:25:35 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{
+                "assets":"@/assets",
+                "components":"@/components",
+                "common":"@/common",
+                "network":"@/network",
+            }
+        }
+    }
+}
